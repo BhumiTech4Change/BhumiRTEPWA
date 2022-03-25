f2b8a02c5ec7ea2c2df7b6038f378446aa55b0c9
deepakchethan <deepakchethan@outlook.com>
2022-03-25 08:10:39 +0530

    Feat: Add skeleton app structure and angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
+import { HomeComponent } from "./components/home/home.component";
+import { FeedbackComponent } from "./components/feedback/feedback.component";
+import { CreditsComponent } from "./components/credits/credits.component";
+import { AuthGuard } from "./guards/auth/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home',  pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'credits', component: CreditsComponent },
+
+  { path: 'home', component: HomeComponent, canActivate: [ AuthGuard ] },
+  { path: 'feedback', component: FeedbackComponent, canActivate: [ AuthGuard ] },
+
+  { path: '**', redirectTo: 'home',  pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutComponent } from './components/about/about.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { HomeComponent } from './components/home/home.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { CreditsComponent } from './components/credits/credits.component';
+import { RestComponent } from './services/rest/rest/rest.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutComponent,
+    ForgotPasswordComponent,
+    HomeComponent,
+    FeedbackComponent,
+    CreditsComponent,
+    RestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
